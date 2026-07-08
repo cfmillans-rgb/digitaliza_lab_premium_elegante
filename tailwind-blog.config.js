67989620ec9f919a30058c072c2cc605e48b0c86
Camila Millán <cfmillans@gmail.com>
2026-07-08 18:57:48 -0400

    Fix: Restore global body tailwind classes

--- tailwind-blog.config.js
+++ tailwind-blog.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./blog.html"],
+  content: ["./blog.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
